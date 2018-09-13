d143bd08be3d7cb6b96510d7e18588ec2ec2afeb
bridgetsmart <43231978+bridgetsmart@users.noreply.github.com>
2018-09-13 16:44:18 +0930

    update person.h

--- Person.h
+++ Person.h
@@ -1,6 +1,30 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <string>
 
+class Person
+{
+
+std::string name; //name of worker
+std::string job; //job
+bool working;  //if they are currently at work
+
+public:
+
+Person();
+Person(std::string name, std::string job, int experience);
+
+//getters
+bool at_work();
+std::string get_name();
+std::string get_job();
+
+//setters
+void set_name(std::string new_name);
+void arrive(int shift_length);
+void leave();
+
+}
 
 #endif PERSON_H
