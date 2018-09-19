9f79beff58a3cbd1ce45b9ed37bacd6963e6a9f3
Christopher-Balnaves <43231848+Christopher-Balnaves@users.noreply.github.com>
2018-09-19 12:45:18 +0930

    Update Plant.h

--- Plant.h
+++ Plant.h
@@ -9,7 +9,7 @@ class Plant {
   int species_id;
   int price;
   int current_stock;
-  int free_space_in_stock;
+  int possible_stock;
   int fertilizing_req;
   int watering_req;
   std::string location;
@@ -17,21 +17,32 @@ class Plant {
   std::string species;
   
 public:
+
+  // No constructors as abstract class
+
+  // Getters
   int get_species_id();
   int get_price();
-  int get_free_space();
+  int get_possible_stock();
   int get_current_stock();
   std::string get_location();
   std::string get_in_or_out();
   std::string get_species();
   void print_information();
-  
+
+  // Setters
   bool set_current_stock(int stock_level);
   bool set_location(std::string new_location);
-
+  bool set_possible_stock(int stock_level);
+  bool set_price(int new_price);
   
+  // Virtual functions
+  virtual void get_watering_req();
+  virtual void get_fertilizing_req();
   virtual bool water_plant()=0;
   virtual bool fertilize_plant()=0;
+
+  // No destructors as abstract class
 
 };
 
