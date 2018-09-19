586f9d1d67f13350e1f2200e82d44467e26c4301
Christopher-Balnaves <43231848+Christopher-Balnaves@users.noreply.github.com>
2018-09-19 13:38:56 +0930

    Create Succulents.h

---
+++ Succulents.h
@@ -0,0 +1,24 @@
+#ifndef SUCCULENTS_H
+#define SUCCULENTS_H value
+
+#include "Plant.h"
+
+class Succulents : public Plant {
+
+public:
+	
+	// Constructors
+	Succulents(std::string species);
+	Succulents(std::string species, int price, int current_stock, int possible_stock, std::string location);
+
+	// Redeclaration of virtual functions
+	void get_watering_req();
+ 	void get_fertilizing_req();
+
+	// Destructor
+	~Succulents();
+
+};
+
+#endif
+
