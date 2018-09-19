221db2c08c4a46c1122e00745dd88fa793084c95
Christopher-Balnaves <43231848+Christopher-Balnaves@users.noreply.github.com>
2018-09-19 12:45:48 +0930

    Update Plant.cpp

--- Plant.cpp
+++ Plant.cpp
@@ -1,6 +1,10 @@
 #include "Plant.h"
 #include <iostream>
 
+// Species_count initialization
+int Plant::species_count=0;
+
+// Getters
 int Plant::get_species_id() {
 	return species_id;
 }
@@ -9,8 +13,8 @@ int Plant::get_price() {
 	return price;
 }
 
-int Plant::get_free_space() {
-	return free_space_in_stock;
+int Plant::get_possible_stock() {
+	return possible_stock;
 }
 
 int Plant::get_current_stock() {
@@ -33,15 +37,25 @@ void Plant::print_information() {
 	std::cout << "For species " << species << " :" << std::endl;
 	std::cout << "\tSpecies ID is " << species_id << "." << std::endl;
 	std::cout << "\tPrice is " << price << " dollars." << std::endl;
-	std::cout << "\tCurrent stock level is " << current_stock << " of " << (current_stock+free_space_in_stock) << "." << std::endl;
+	std::cout << "\tCurrent stock level is " << current_stock << " of " << possible_stock) << "." << std::endl;
 	std::cout << "\tLocation is " << location << "." << std::endl;
 	std::cout << "\t" << species << " must be kept " << in_or_out << "." << std::endl;
 	return;
 }
 
+// Setters
 bool Plant::set_current_stock(int stock_level) {
 	current_stock=stock_level;
 	if (current_stock==stock_level) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool Plant::set_possible_location(int stock_level) {
+	possible_stock=stock_level;
+	if (possible_stock==stock_level) {
 		return true;
 	} else {
 		return false;
@@ -55,4 +69,41 @@ bool Plant::set_location(std::string new_location) {
 	} else {
 		return false;
 	}
+}
+
+bool Plant::set_price(int new_price) {
+	price=new_price;
+	if (price==new_price) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// Virtual functions
+void Plant::get_watering_req() {
+	std::cout << "Water " << species;
+	switch (watering_req) {
+		case 1:
+			std::cout << " once a week." << endl;
+			break;
+		case 2:
+			std::cout << " twice a week." << endl;
+			break;
+		case 3:
+			std::cout << " three times a week." << endl;
+	}
+	
+}
+
+void Plant::get_fertilizing_req() {
+	std::cout << "Fertilize " << species;
+	switch (fertilizing_req) {
+		case 1:
+			std::cout << " once a year." << endl;
+			break;
+		case 2:
+			std::cout << " twice a year." << endl;
+	}
+	
 }
