f57313268a96b7e268db14fb4765577bc0279922
Christopher-Balnaves <43231848+Christopher-Balnaves@users.noreply.github.com>
2018-09-19 11:37:44 +0930

    Update Plant.h

--- Plant.h
+++ Plant.h
@@ -7,24 +7,31 @@ class Plant {
 
   static int species_count;     // Count of number of species in nursery - used for inventory array allocation
   int species_id;
-  int fertilizing_req;
-  int watering_req;
   int price;
   int current_stock;
   int free_space_in_stock;
+  int fertilizing_req;
+  int watering_req;
   std::string location;
   std::string in_or_out;
   std::string species;
   
 public:
+  int get_species_id();
+  int get_price();
+  int get_free_space();
   int get_current_stock();
   std::string get_location();
+  std::string get_in_or_out();
+  std::string get_species();
+  void print_information();
   
   bool set_current_stock(int stock_level);
   bool set_location(std::string new_location);
+
   
-  virtual water_plant=0;
-  virtual fertilize_plant=0;
+  virtual bool water_plant()=0;
+  virtual bool fertilize_plant()=0;
 
 };
 
